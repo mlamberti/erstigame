da3c3f658c36a91f9ae404fac4b4d35dd94767c3
Michael Lamberti <michael.lamberti@devolo.de>
2019-03-13 22:06:58 +0100

    generate user list with ngFor

---
+++ src/app/MOCK-USERS.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 1, name: 'Ben' },
+  { id: 2, name: 'Tim' },
+  { id: 3, name: 'Knut' },
+  { id: 14, name: 'Eva' },
+  { id: 15, name: 'Maria' },
+  { id: 16, name: 'Lisa' },
+  { id: 17, name: 'Lisa' },
+  { id: 18, name: 'Joanna' },
+  { id: 19, name: 'Dieter' },
+  { id: 20, name: 'Peter' }
+];
