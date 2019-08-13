3100d7cdda5f70ca97e8ef92a6f179ab15383aab
Ben Schuermann <bennyschuermann@gmx.net>
2019-08-13 12:00:20 +0200

    cards für gruppenübersicht

--- src/app/MOCK-USER.ts
+++ src/app/MOCK-USER.ts
@@ -4,11 +4,11 @@ export const USER: user[] = [
   { id: 1, ip:1, name: 'Ben', info: 'keine',icon:'trophy', active: true},
   { id: 2, ip:1, name: 'Tim', info: 'keine' ,icon:'hammer', active: true},
   { id: 3, ip:1, name: 'Knut', info: 'keine' ,icon:'infinite', active: true},
-  { id: 14, ip:1, name: 'Eva', info: 'keine' ,icon:'pulse', active: true},
-  { id: 15, ip:1, name: 'Maria', info: 'keine' ,icon:'airplane', active: true},
-  { id: 16, ip:1, name: 'Lisa', info: 'keine' ,icon:'airplane', active: true},
-  { id: 17, ip:1, name: 'Lisa', info: 'keine' ,icon:'airplane', active: true},
-  { id: 18, ip:1, name: 'Joanna', info: 'keine' ,icon:'airplane', active: true},
-  { id: 19, ip:1, name: 'Dieter', info: 'keine' ,icon:'heart-dislike', active: true},
-  { id: 20, ip:1, name: 'Peter', info: 'keine' ,icon:'airplane', active: true}
+  { id: 4, ip:1, name: 'Eva', info: 'keine' ,icon:'pulse', active: true},
+  { id: 5, ip:1, name: 'Maria', info: 'keine' ,icon:'airplane', active: true},
+  { id: 6, ip:1, name: 'Lisa', info: 'keine' ,icon:'airplane', active: true},
+  { id: 7, ip:1, name: 'Lisa', info: 'keine' ,icon:'airplane', active: true},
+  { id: 8, ip:1, name: 'Joanna', info: 'keine' ,icon:'airplane', active: true},
+  { id: 9, ip:1, name: 'Dieter', info: 'keine' ,icon:'heart-dislike', active: true},
+  { id: 10, ip:1, name: 'Peter', info: 'keine' ,icon:'airplane', active: true}
 ];
