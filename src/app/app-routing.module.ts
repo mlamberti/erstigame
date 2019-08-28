cb5b7954c5bed8b9ef4d6ee59b6896877e054e31
Michael Lamberti <michael.lamberti@devolo.de>
2019-08-29 01:37:11 +0200

    webui: add registry page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'camera', loadChildren: './camera/camera.module#CameraPageModule' },
-  { path: 'hashtag-info', loadChildren: './hashtag-info/hashtag-info.module#HashtagInfoPageModule' }
+  { path: 'hashtag-info', loadChildren: './hashtag-info/hashtag-info.module#HashtagInfoPageModule' },
+  { path: 'registry', loadChildren: './registry/registry.module#RegistryPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/registry/registry.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegistryPage } from './registry.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RegistryPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [RegistryPage]
+})
+export class RegistryPageModule {}
