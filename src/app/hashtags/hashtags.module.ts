18bb191b55675e81d331b6239804d81936043cb6
Michael Lamberti <michael.lamberti@devolo.de>
2019-09-28 03:04:38 +0200

    floor hours instead of rounding

--- src/app/hashtags/hashtags.module.ts
+++ src/app/hashtags/hashtags.module.ts
@@ -5,13 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HashtagsPage } from './hashtags.page';
 import { ExpandableComponent } from "../components/expandable/expandable.component";
-
+import { NgMathPipesModule } from 'ngx-pipes';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    NgMathPipesModule,
     RouterModule.forChild([{ path: '', component: HashtagsPage }])
   ],
   declarations: [HashtagsPage,ExpandableComponent]
